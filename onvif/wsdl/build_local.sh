ba8db587ff54d60bc5ceec4a5ca8e51cc9b11306
Yang Zhen <33373689@qq.com>
2017-11-27 21:07:22 +0800

    c++

--- onvif/wsdl/build_local.sh
+++ onvif/wsdl/build_local.sh
@@ -2,5 +2,5 @@
 
 cd csdn-gsoap-wsdl
 
-wsdl2h -o ../onvif.h -c -s -t ./typemap.dat devicemgmt.wsdl media.wsdl event.wsdl display.wsdl deviceio.wsdl imaging.wsdl ptz.wsdl receiver.wsdl recording.wsdl search.wsdl remotediscovery.wsdl replay.wsdl analytics.wsdl analyticsdevice.wsdl actionengine.wsdl accesscontrol.wsdl doorcontrol.wsdl
+wsdl2h -o ../onvif.h -c++ -s -t ./typemap.dat devicemgmt.wsdl media.wsdl event.wsdl display.wsdl deviceio.wsdl imaging.wsdl ptz.wsdl receiver.wsdl recording.wsdl search.wsdl remotediscovery.wsdl replay.wsdl analytics.wsdl analyticsdevice.wsdl actionengine.wsdl accesscontrol.wsdl doorcontrol.wsdl
 
