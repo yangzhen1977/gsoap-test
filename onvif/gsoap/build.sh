408afef3a6c8f4f321e59464ccf99596ac105e6e
Yang Zhen <33373689@qq.com>
2017-11-27 21:18:29 +0800

    build gsoap c,c++,c++11

--- onvif/gsoap/build.sh
+++ onvif/gsoap/build.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-soapcpp2 -c++  ../wsdl/onvif.h -I /usr/share/gsoap/import -I /usr/share/gsoap
+(
+mkdir c 2>/dev/null
+cd c &&  soapcpp2 -c  ../../wsdl/onvif.h -I /usr/share/gsoap/import -I /usr/share/gsoap
+)
+
+(
+mkdir cpp 2>/dev/null
+cd cpp &&  soapcpp2 -c++  ../../wsdl/onvif.h -I /usr/share/gsoap/import -I /usr/share/gsoap
+)
+
+(
+mkdir c11 2>/dev/null
+cd c11 &&  soapcpp2 -c++11  ../../wsdl/onvif.h -I /usr/share/gsoap/import -I /usr/share/gsoap
+)
