f558671b32f93b4f2ac7e4c2500a96540565db18
Zhen Yang <yangzhen1977@outlook.com>
2017-12-08 20:16:30 +0800

    added -j to soapcpp2

--- probe/gsoap/build.sh
+++ probe/gsoap/build.sh
@@ -5,7 +5,7 @@ echo "                       gsoap c"
 mkdir c 2>/dev/null
 rm -fr c/*
 cd c || exit 1
-soapcpp2 -c  ../../wsdl/onvif-c.h      -x -I /usr/share/gsoap/import -I /usr/share/gsoap 
+soapcpp2 -c  ../../wsdl/onvif-c.h      -x -j -I /usr/share/gsoap/import -I /usr/share/gsoap 
 echo "                       gosap c succes"
 ) || exit 1
 
@@ -15,7 +15,7 @@ echo "                       gsoap c++"
 mkdir cpp 2>/dev/null
 rm -fr cpp/*
 cd cpp || exit 1
-soapcpp2 -c++ ../../wsdl/onvif-cpp.h   -x -I /usr/share/gsoap/import -I /usr/share/gsoap 
+soapcpp2 -c++ ../../wsdl/onvif-cpp.h   -x -j -I /usr/share/gsoap/import -I /usr/share/gsoap 
 echo "                       gosap c++ succes"
 ) || exit 1
 
@@ -25,6 +25,6 @@ echo "                       gsoap c++11"
 mkdir c11 2>/dev/null
 rm -fr c11/*
 cd c11 || exit 1
-soapcpp2 -c++11 ../../wsdl/onvif-c11.h  -x -I /usr/share/gsoap/import -I /usr/share/gsoap 
+soapcpp2 -c++11 ../../wsdl/onvif-c11.h  -x -j -I /usr/share/gsoap/import -I /usr/share/gsoap 
 echo "                       gosap c++11 succes"
 ) || exit 1
